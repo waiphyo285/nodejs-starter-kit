fe1da40df0f1ebfa8156cd04fefd05c243279a6b
waiphyo-d3 <wai.phyo@d3-sg.com>
2022-09-29 17:51:31 +0630

    init mulit language in dashboard

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(genRouter);
 app.use("/d-mar", tokenRouter);
 
 // connect to api routes
-app.use("/api", apiRouter);
+app.use("/api", verifyToken, apiRouter);
 
 // connect to file routes
 app.use("/file", verifyToken, fileRouter);
